f1fa99bad3e424da2cc0e03e65da68c4a5d47a73
Rishabh Nirmalkar <iamrishabhnirmalkar@gmail.com>
2023-06-21 11:40:56 +0530

    init

--- Project01/script.js
+++ Project01/script.js
@@ -1,2 +1,22 @@
-const btn = document.querySelectorAll("button");
-console.log(btn);
+const btn = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+btn.forEach(function (btn) {
+  console.log(btn);
+  btn.addEventListener("click", function (e) {
+    console.log(e);
+    console.log(e.target);
+    if (e.target.id === "grey") {
+      body.style.background = e.target.id;
+    }
+    if (e.target.id === "purple") {
+      body.style.background = e.target.id;
+    }
+    if (e.target.id === "blue") {
+      body.style.background = e.target.id;
+    }
+    if (e.target.id === "yellow") {
+      body.style.background = e.target.id;
+    }
+  });
+});
